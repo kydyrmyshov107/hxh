d9874ca08f87daa5acfc95fa697a91498d860476
Nursultan <kydyrmysovnursultan@gmail.com>
2024-10-03 01:05:22 +0600

    music is not work

--- src/components/pages/card/Card.jsx
+++ src/components/pages/card/Card.jsx
@@ -7,17 +7,16 @@ const Card = () => {
   return (
     <div>
    <h1>Hunter X Hunter</h1>
-    <div className={style.Card}>
+    <div className={style.Card}>  
       {data?.map((item) => (
         <div key={item.id} className={style.content}>
           <img className={style.image} src={item.img} alt={item.name} />
           <p>{item.name}</p>
           <h3>{item.also_known_as}</h3>
           <p>{item.abilities}</p>
-          <h4      style={{
-                color: item.state === 'alive' ? 'green' : 'red',
-                fontWeight: 'bold',
-              }}>{item.state}</h4>
+          <h4 style={{
+          color: item.state === 'alive' ? 'green' : 'red',
+          fontWeight: 'bold'}}>{item.state}</h4>
         </div>
       ))}
     </div>
